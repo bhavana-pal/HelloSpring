7dd88b33e4289207c91e6dcb46374950de903b0d
bhavna <bhavanapal14@yahoo.com>
2017-02-08 19:42:15 +0530

    partial task done

--- HelloSpring/src/main/java/com/niit/HelloSpring/controller/HelloController.java
+++ HelloSpring/src/main/java/com/niit/HelloSpring/controller/HelloController.java
@@ -2,12 +2,27 @@ package com.niit.HelloSpring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 	@RequestMapping(value="/")
-	public String printView(){
-		return "index";
+	public ModelAndView index(){
+		ModelAndView mv=new ModelAndView("index");
+		mv.addObject("ifUserClickedIndex",true);
+		return mv;
+	}
+	@RequestMapping(value="/about")
+	public ModelAndView about(){
+		ModelAndView mv=new ModelAndView("about");
+		mv.addObject("ifUserClickedAbout",true);
+		return mv;
+	}
+	@RequestMapping(value="/contact")
+	public ModelAndView contact(){
+		ModelAndView mv=new ModelAndView("contact");
+		mv.addObject("ifUserClickedContact",true);
+		return mv;
 	}
 	
 
---
+++ HelloSpring/src/main/webapp/WEB-INF/view/shared/header.jsp
@@ -0,0 +1,11 @@
+<nav id="menu-left">
+			<ul>
+				<li><a href="index.html">Home</a></li>
+				<li><a href="products.html">products</a></li>
+				<li><a href="about.html">about</a></li>
+				<li><a href="index.html">pages</a></li>
+				<li><a href="blog.html">blog</a></li>
+				<li><a href="contact.html">Contact</a></li>
+				<div class="clear"></div>
+			</ul>
+		</nav>
